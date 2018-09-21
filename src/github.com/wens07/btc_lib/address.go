8a2c8b867a7afdb03d9176ffb3817e8ba0a9945d
wens.wq <wens.wq@gmail.com>
2018-09-21 16:37:09 +0800

    add ecdsa files

--- src/github.com/wens07/btc_lib/address.go
+++ src/github.com/wens07/btc_lib/address.go
@@ -3,7 +3,7 @@
  *
  * Copyright © 2015--2018 . All rights reserved.
  *
- * File: ecdsa.go, Date: 2018-09-19
+ * File: address.go, Date: 2018-09-19
  *
  *
  * This library is free software under the terms of the GNU General Public License
@@ -14,10 +14,57 @@
 
 package btc_lib
 
-import "github.com/btcsuite/btcd/btcec"
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcutil/base58"
+)
+
+const (
+	MAIN_VERSION_P2PKH = 0
+	MAIN_VERSION_P2SH  = 5
+	MAIN_VERSION_WIF   = 128
+
+	TEST_VERSION_P2PKH = 111
+	TEST_VERSION_P2SH  = 196
+	TEST_VERSION_WIF   = 239
+)
+
+type AddressDetail struct {
+	Ver    string `json:"version"`
+	Rip160 string `json:"ripemd160"`
+
+	Ck string `json:"checksum"`
+}
 
 func GetSecp256k1Params() *btcec.KoblitzCurve {
 
 	return btcec.S256()
+
+}
+
+func DecodeAddress(addr string) ([]byte, error) {
+
+	outstr := hex.EncodeToString(base58.Decode(addr))
+	//ver, err := strconv.ParseInt(outstr[0:2], 16, 64)
+	//if err != nil {
+	//	return "", fmt.Errorf("in DecodeAddress function, get version error: %v", err)
+	//}
+
+	res := AddressDetail{
+		outstr[0:2],
+		outstr[2:42],
+		outstr[42:],
+	}
+
+	jsonres, err := json.Marshal(res)
+	if err != nil {
+		return nil, fmt.Errorf("in DecodeAddress function, get json error: %v", err)
+	}
+
+	return jsonres, nil
 
 }
